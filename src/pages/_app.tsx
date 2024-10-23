de683cba8107265994425e558c694f43c7c5a2db
cotlaswebhost <cotlaswebhost@gmail.com>
2024-10-23 23:02:49 +0530

    Added code script added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -58,3 +58,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
+//implementation 
+<Script
+  id="Absence-banner"
+  async
+  strategy="afterInteractive"
+  onError={(e) => {
+    console.error('Script failed to load', e);
+  }}
+  src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`}
+  crossOrigin="anonymous"
+/>
